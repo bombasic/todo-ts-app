0f928f54f1b8ffa329fb620c2d51880520aa29cb
Alen Basic <alenbasic3@gmail.com>
2022-04-01 16:46:22 -0400

    Add useRef to NewTodo to take users input

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,12 +1,18 @@
+import { useRef } from "react";
+
 const NewTodo = () => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
+
+    const enteredText = todoTextInputRef.current!.value;
   };
 
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor="text">ToDo Text</label>
-      <input type="text" id="text" />
+      <input type="text" id="text" ref={todoTextInputRef} />
       <button>Add ToDo</button>
     </form>
   );
