f29f21684e5fab5e24f235a244ab2c5dbef58862
Alen Basic <alenbasic3@gmail.com>
2022-04-01 17:18:44 -0400

    removing dummy data and adding useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
+import { useState } from "react";
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 
 function App() {
-  const todo = [
-    new Todo("Learn React"),
-    new Todo("Learn TypeScript"),
-    new Todo("Get a Job ;)"),
-  ];
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
 
   return (
     <div>
-      <NewTodo />
-      <Todos items={todo} />
+      <NewTodo onAddTodo={addTodoHandler} />
+      <Todos items={todos} />
     </div>
   );
 }
